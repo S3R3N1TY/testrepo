a4791c88d56fa058166ed81ecc1d3387a9cc2d82
S3R3N1TY <aaronmorganjones@gmail.com>
2026-02-19 21:16:57 +0000

    Tweaked ECS boundaries

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,14 @@
 #include <Engine.h>
-
 #include "Simulation.h"
 
 int main()
 {
     Simulation simulation{};
     Engine engine{};
-    engine.run(simulation);
+
+    Engine::RunConfig cfg{};
+    cfg.vertexShaderPath = "shaders/triangle.vert.spv";
+    cfg.fragmentShaderPath = "shaders/triangle.frag.spv";
+
+    engine.run(simulation, cfg);
 }
